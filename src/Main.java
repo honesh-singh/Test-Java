f31f3b3861a09a528a10761a3d252c6c3d1e4518
honesh singh <honeshsingh91@gmail.com>
2025-09-25 12:28:37 +0530

    new add

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+
 public class Main {
     public static void main(String[] args) {
+         List<Integer> list = Arrays.asList(5,6,534,5,34,534,5);
+        Comparator<Integer> com = (a,b)-> b-a;
+
+        System.out.println("original list -> "+list);
+        Collections.sort(list);
+        System.out.println("after sort -> "+list);
+        Collections.sort(list,com);
+        System.out.println("sort using comparator -> "+list);
+
 
     }
 }
