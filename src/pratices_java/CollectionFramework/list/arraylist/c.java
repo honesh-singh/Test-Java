66a47c2b490305b8f28a8842a3e377024c603fe1
honesh singh <honeshsingh91@gmail.com>
2025-09-26 23:28:06 +0530

    practice........................

---
+++ src/pratices_java/CollectionFramework/list/arraylist/c.java
@@ -0,0 +1,26 @@
+package pratices_java.CollectionFramework.list.arraylist;
+
+// Basic Operations
+// Create an ArrayList<Integer> and perform the following:
+
+// Add 5 numbers
+
+// Remove the 3rd element
+
+// Update the 2nd element to a new number
+
+// Print the list after each operation
+import java.util.ArrayList;
+public class c{
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.remove(3);
+        list.set(2,33);
+        System.out.println(list);
+    }
+}
