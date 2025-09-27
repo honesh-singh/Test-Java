9eaae7819c82984db956b81769b2900551dc6759
honesh singh <honeshsingh91@gmail.com>
2025-09-27 21:39:56 +0530

    method reference .....

---
+++ src/pratices_java/FunctionalProgramming/MethodReferences/StaticMethodReference2.java
@@ -0,0 +1,17 @@
+package pratices_java.FunctionalProgramming.MethodReferences;
+
+import java.util.Arrays;
+import java.util.List;
+// if your lambda just calling an existing method, replace it with
+// method reference
+public class StaticMethodReference2 {
+
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("harsha","naman","vardhan");
+        System.out.println("by lambda ->");
+        list.forEach(name -> System.out.println(name));// warning here : Lambda can be replaced with method reference (hosiyaar hai system)
+        System.out.println("by method ref ->");
+        list.forEach(System.out::println);
+
+    }
+}
