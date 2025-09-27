0358d4b407d1fd3f5652a5af57ed15e729ab9422
honesh singh <honeshsingh91@gmail.com>
2025-09-27 21:27:47 +0530

    method reference .....

---
+++ src/pratices_java/FunctionalProgramming/MethodReferences/StaticMethodReference.java
@@ -0,0 +1,20 @@
+package pratices_java.FunctionalProgramming.MethodReferences;
+
+import java.util.function.Function;
+
+public class StaticMethodReference {
+
+    public static int square(int num){
+        return num * num;
+    }
+
+    public static void main(String[] args) {
+        Function<Integer,Integer> fun = n -> square(n);
+        System.out.println("by lambda expression -> "+fun.apply(5));
+
+
+        Function<Integer,Integer> func = StaticMethodReference::square;
+        System.out.println("by method reference -> "+func.apply(5));
+    }
+
+}
